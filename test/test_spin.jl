49fb5ea338f20ae31363a4961995d11c3dd1ebcb
Erik Schnetter <schnetter@gmail.com>
2021-03-13 11:05:06 -0500

    Add test case for Laplacian

--- test/test_spin.jl
+++ test/test_spin.jl
@@ -19,3 +19,33 @@
     F″ = spinsph_evaluate(C′, s)
     @test F″ ≈ F′
 end
+
+@testset "Laplacian" begin
+    lmax = 100
+
+    N = lmax + 1
+    Θ, Φ = sph_points(N)
+    @test length(Θ) == N
+    M = length(Φ)
+
+    # Calculate Laplacian directly
+    F = randn(N, M)
+    C = sph_transform(F)
+    ΔC = sph_laplace(C)
+    ΔF = sph_evaluate(ΔC)
+
+    # Calculate Laplacian via eth and ethbar
+    F⁰ = Complex.(F)
+    C⁰ = spinsph_transform(F⁰, 0)
+    ðC¹ = spinsph_eth(C⁰, 0)
+    ð̄ðC⁰ = spinsph_ethbar(ðC¹, 1)
+    ð̄ðF⁰ = spinsph_evaluate(ð̄ðC⁰, 0)
+    @test ð̄ðF⁰ ≈ ΔF
+
+    # Calculate Laplacian via ethbar and eth
+    ð̄C⁻¹ = spinsph_ethbar(C⁰, 0)
+    ðð̄C⁰ = spinsph_eth(ð̄C⁻¹, -1)
+    ðð̄F⁰ = spinsph_evaluate(ðð̄C⁰, 0)
+
+    @test ðð̄F⁰ ≈ ΔF
+end
