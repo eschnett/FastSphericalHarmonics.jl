47c8ff5be52c04568f7822d22e614033d7d26690
Erik Schnetter <schnetter@gmail.com>
2022-01-28 18:01:59 -0500

    Convert signs for AbstractSphericalHarmonics API

--- src/abstract.jl
+++ src/abstract.jl
@@ -53,40 +53,56 @@ function ash_mode_index(s::Integer, l::Integer, m::Integer, lmax::Integer)
     return spinsph_mode(s, l, m)
 end
 
+function change_signs!(flm::AbstractArray{<:Complex}, s::Integer)
+    for (row, col) in CartesianIndices(flm)
+        m = col == 1 ? 0 : (col % 2 == 0 ? -1 : 1) * (col ÷ 2)
+        l = row + max(abs(s), abs(m)) - 1
+        sign = (isodd(s) && m < -s) || (m >= -s && isodd(m)) ? -1 : +1
+        flm[row, col] = flipsign(flm[row, col], sign)
+    end
+    return flm
+end
+
 export ash_transform!, ash_transform, ash_evaluate!, ash_evaluate
 function ash_transform!(flm::AbstractArray{<:Complex},
                         f::AbstractMatrix{<:Complex}, s::Integer, lmax::Integer)
     flm .= f
-    return spinsph_transform!(flm, s)
+    spinsph_transform!(flm, s)
+    return change_signs!(flm, s)
 end
 function ash_transform(f::AbstractMatrix{<:Complex}, s::Integer, lmax::Integer)
-    return spinsph_transform(f, s)
+    return spinsph_transform!(similar(f), f, s)
 end
 
 function ash_evaluate!(f::AbstractMatrix{<:Complex},
                        flm::AbstractArray{<:Complex}, s::Integer, lmax::Integer)
     f .= flm
+    change_signs!(f, s)
     return spinsph_evaluate!(f, s)
 end
 function ash_evaluate(flm::AbstractArray{<:Complex}, s::Integer, lmax::Integer)
-    return spinsph_evaluate(flm, s)
+    return ash_evaluate!(similar(flm), flm, s)
 end
 
 export ash_eth!, ash_eth, ash_ethbar!, ash_ethbar
 function ash_eth!(ðflm::AbstractArray{<:Complex}, flm::AbstractArray{<:Complex},
                   s::Integer, lmax::Integer)
+    change_signs!(flm, s)
     ðflm .= spinsph_eth(flm, s)
-    return ðflm
+    change_signs!(flm, s)
+    return change_signs!(ðflm, s + 1)
 end
 function ash_eth(flm::AbstractArray{<:Complex}, s::Integer, lmax::Integer)
-    return spinsph_eth(flm, s)
+    return ash_eth(similar(flm), flm, s)
 end
 
 function ash_ethbar!(ðflm::AbstractArray{<:Complex},
                      flm::AbstractArray{<:Complex}, s::Integer, lmax::Integer)
+    change_signs!(flm, s)
     ð̄flm .= spinsph_ethbar(flm, s)
-    return ð̄flm
+    change_signs!(flm, s)
+    return change_signs!(ð̄flm, s - 1)
 end
 function ash_ethbar(flm::AbstractArray{<:Complex}, s::Integer, lmax::Integer)
-    return spinsph_ethbar(flm, s)
+    return ash_ethbar(similar(flm), flm, s)
 end
